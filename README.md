a229447aab754672ca322ecb4b72202c32bb318e
Yijing Chen <yijingch@umich.edu>
2020-08-19 15:28:50 -0400

    add parsing code

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## Pre-processing / Analysis Documentations
 
-CODE FILES:
+##### Code files:
 
 1. `crawl_reddit_fixstart_by_year.py`
-- Action: crawl submissions and comments from subreddits listed in `index/left-right-labels.csv` within a specified time interval into
+- crawl submissions and comments from subreddits listed in `index/left-right-labels.csv` within a specified time interval into
 - e.g., `python3 aggr_per_subm_year.py --start_year=2012 --end_year=2021`
 
 -------------------------------------------------
@@ -32,4 +32,36 @@ CODE FILES:
         |     |      |--- 2020
 -------------------------------------------------
 
-2. 
+
+2. `reset_db.py` + `parse_json_to_db.py`
+- parse relevant data points into a SQLite database
+-------------------------------------------------
+    CREATE TABLE "submission" (
+          "id" TEXT PRIMARY KEY,
+          "author" TEXT,
+          "author_fullname" TEXT,
+          "subreddit" TEXT,
+          "title" TEXT,
+          "selftext" TEXT,
+          "score" INTEGER,
+          "created_utc" TEXT
+          )
+
+    CREATE TABLE "comment" (
+            "id" TEXT PRIMARY KEY,
+            "author" TEXT,
+            "author_fullname" TEXT,
+            "subreddit" TEXT,
+            "parent_id" TEXT,
+            "link_id" TEXT,
+            "body" TEXT,
+            "score" INTEGER,
+            "created_utc" TEXT
+            )
+-------------------------------------------------
+
+
+##### Index/descriptive files:
+
+1. `index/left-right-labels.csv`: a list of subreddits we crawl contents from
+2. `/index/descriptives-from-2012.csv`: number of submissions/comments crawled from each subreddit in each year
---
+++ reset_db.py
@@ -0,0 +1,103 @@
+## --------------------------- ##
+## ---- Prepare SQLite DB ---- ##
+## --------------------------- ##
+
+import sqlite3
+
+DBNAME = "reddit-db-from-2012-2014-small.db"
+DBPATH = "data/"
+
+def reset_db():
+
+
+    # 1. try to create DB
+    try:
+        conn = sqlite3.connect(DBPATH + DBNAME)
+        cur = conn.cursor()
+    except Exception as e:
+        print("Error creating DB:", e)
+        conn.close()
+
+
+    # 2. drop tables if exists
+    try:
+        statement = """
+        DROP TABLE IF EXISTS "subreddit";
+        """
+        cur.execute(statement)
+        conn.commit()
+    except Exception as e:
+        print("Error dropping TABLE subreddit:", e)
+
+    try:
+        statement = """
+        DROP TABLE IF EXISTS "submission";
+        """
+        cur.execute(statement)
+        conn.commit()
+    except Exception as e:
+        print("Error dropping TABLE submission:", e)
+
+    try:
+        statement = """
+        DROP TABLE IF EXISTS "comment";
+        """
+        cur.execute(statement)
+        conn.commit()
+    except Exception as e:
+        print("Error dropping TABLE comment:", e)
+
+
+    # 3. create new tables
+    try:
+        statement = """
+        CREATE TABLE "subreddit" (
+        "id" TEXT PRIMARY KEY,
+        "name" TEXT,
+        "label" INTEGER
+        );
+        """
+        cur.execute(statement)
+        conn.commit()
+    except Exception as e:
+        print("Error creating TABLE subreddit:", e)
+
+    try:
+        statement = """
+        CREATE TABLE "submission" (
+        "id" TEXT PRIMARY KEY,
+        "author" TEXT,
+        "author_fullname" TEXT,
+        "subreddit" TEXT,
+        "title" TEXT,
+        "selftext" TEXT,
+        "score" INTEGER,
+        "created_utc" TEXT
+        );
+        """
+        cur.execute(statement)
+        conn.commit()
+    except Exception as e:
+        print("Error creating TABLE submission:", e)
+
+    try:
+        statement = """
+        CREATE TABLE "comment" (
+        "id" TEXT PRIMARY KEY,
+        "author" TEXT,
+        "author_fullname" TEXT,
+        "subreddit" TEXT,
+        "parent_id" TEXT,
+        "link_id" TEXT,
+        "body" TEXT,
+        "score" INTEGER,
+        "created_utc" TEXT
+        );
+        """
+        cur.execute(statement)
+        conn.commit()
+    except Exception as e:
+        print("Error creating TABLE comment:", e)
+
+if __name__ == "__main__":
+    reset_db()
